ef7f3a7dbb70dbddf3e5df62aa7145293171a6c6
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2013-03-06 22:50:04 +0100

    Better type for metapost things

--- MarXup/MetaPost.hs
+++ MarXup/MetaPost.hs
@@ -1,27 +1,75 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving, OverloadedStrings #-}
 
 module MarXup.MetaPost where
 
 import MarXup.Tex
-
+import MarXup.MultiRef
+import Control.Monad.Reader
+import Control.Applicative
+import GHC.Exts( IsString(..) )
+import Data.Monoid
+import System.FilePath
   
-metaPostPreamble :: Tex a -> Tex ()
+newtype MP a = MP (Multi a)
+  deriving (Monad, MonadFix, Applicative, Functor)
+
+instance IsString (MP ()) where
+   fromString = MP . Raw
+
+instance Monoid (MP()) where  
+  mempty = MP $ Raw ""
+  mappend = (>>)
+
+mpRawLines :: [String] -> MP ()
+mpRawLines ls = MP $ Raw $ concat $ map (++"\n") ls 
+
+mpRefer l = sho =<< MP (Refer l)
+mpLabel  = MP $ Label
+
+sho :: Show a => a -> MP ()
+sho = MP . Raw . show
+                             
+
+mpRawTex :: Tex a -> MP a
+mpRawTex (Tex t) = MP (runReaderT t "<in metapost>")
+
+metaPostPreamble :: Tex a -> MP ()
 metaPostPreamble texPreamble =  do
-  texLines ["outputtemplate := \"%j-%c.mps\";",
+  mpRawLines ["outputtemplate := \"%j-%c.mps\";",
             "input drv;",
             "verbatimtex %&latex",
             ""]
-  texPreamble
-  texLines ["",
+  mpRawTex texPreamble
+  mpRawLines ["",
             "\\begin{document}",
             "etex;",
             "prologues:=3;"]  
   
-metaPostEpilogue = texLn "end"
+metaPostEpilogue = mpRawLines ["end"]
     
 
-metapostFigure :: Label -> Tex a -> Tex ()
-metapostFigure lab t = Metapost $ do
-  Tex "beginfig(" >> Refer lab >> Tex ")\n"
+mkfig :: Label -> MP () -> MP ()
+mkfig lab (MP t) = MP $ do
+  Raw "beginfig(" >> Refer lab >> Raw ")\n"
   t
-  texLn "endfig;"
+  Raw "endfig;\n"
 
+inMP :: MP a -> Tex a
+inMP (MP mp) = do
+ fname <- ask
+ Tex $ lift $ Target (fname <.> "mp") mp
+
+mpQuote :: TeX -> MP ()
+mpQuote t = "\"" <> mpRawTex t <> "\""
+
+
+createMetaPostFigure :: Label -> MP () -> Tex ()
+createMetaPostFigure lab t = do 
+  inMP $ mkfig lab t
+
+includeMetaPostFigure :: [String] -> Label -> TeX
+includeMetaPostFigure opts l = do
+  fname <- ask 
+  let figname = fname ++ "-" ++ show l ++ ".mps"
+  cmd' "includegraphics" opts (tex figname)
+    
--- MarXup/Tex.hs
+++ MarXup/Tex.hs
@@ -2,127 +2,66 @@
 
 module MarXup.Tex where
 
-import Control.Monad.Fix
-import Control.Monad.RWS.Lazy
+import Control.Monad.Reader
 import Control.Applicative
-import Data.Either
 import GHC.Exts( IsString(..) )
 import System.FilePath
 import System.Environment
 import Data.List (intersperse)
+import MarXup.MultiRef
+import Data.Monoid
+
+
+newtype Tex a = Tex (ReaderT FilePath Multi a)
+  deriving (Monad, MonadFix, Applicative, Functor, MonadReader FilePath)
 
 ---------------------------------
--- Marchup interface
-textual = Tex  -- TODO: escape \, &, etc.
+-- MarXup interface
+textual :: String -> TeX
+textual s = Tex $ lift (Raw s)  -- FIXME: escape \, &, etc.
 text = textual 
 
 
 element :: Tex a -> Tex a
 element = id
 
------------------------------------
--- Basic datatype and semantics
-type Label = Int
-
-data Tex a where
-  Return :: a -> Tex a
-  Bind :: Tex a -> (a -> Tex b) -> Tex b
-
-  Tex :: String -> Tex () -- ^ raw TeX code
-
-  -- Reference management
-  Label :: Tex Label
-  Refer :: Label -> Tex Label
-  MFix :: (a -> Tex a) -> Tex a 
-
-  -- MetaPOST
-  Metapost :: Tex a -> Tex a -- the output should go in the metapost file
-  MPFigure :: Label -> Tex () -- inclusion of a metapost figure
-  
-tex = Tex
+tex :: String -> TeX
+tex = Tex . lift . Raw
 type TeX = Tex ()
 
-newLabel = Label  
-reference = Refer
+newLabel :: Tex Label
+newLabel = Tex $ lift Label  
+reference = Tex . lift . Refer
 
-instance MonadFix Tex where
-  mfix = MFix
-
-instance Monad Tex where
-  (>>=) = Bind
-  return = Return
-  
-instance Applicative Tex where
-  (<*>) = ap
-  pure = Return
-  
-instance Functor Tex where
-  fmap = liftM
-  
-instance Monoid (Tex ()) where  
-  mempty = Tex ""
+instance Monoid (TeX) where  
+  mempty = text ""
   mappend = (>>)
   
-instance IsString (Tex ()) where  
-  fromString = Tex
+instance IsString (TeX) where  
+  fromString = text
   
-type References = Int -- how many labels have been allocated
-emptyRefs :: References
-emptyRefs = 0
-                  
-select :: Bool -> a -> Either a a
-select True x = Left x
-select False x = Right x
-            
-
-type TexAndMPost = [Either String String]
-
-newtype Displayer a = Displayer {fromDisplayer :: RWS () TexAndMPost References a }
-  deriving (Monad, MonadWriter TexAndMPost, MonadState References, MonadFix)
--- | Produces a list of strings (either for LaTeX or MetaPOST).
-display :: String -> Bool -> Tex a -> Displayer a 
-display fname mp t = case t of
-      (Tex s) -> tell' s
-      (Return a) -> return a
-      (Bind k f) -> rec k >>= (rec . f)
-      Label -> do x <- get;  put $ x + 1; return x
-      (Refer x) -> do tell [select mp $ show x]; return x
-      (MFix f) -> mfix (rec . f)
-      (Metapost x) -> display fname True x 
-      (MPFigure l) -> tell' $ fname ++ "-" ++ show l ++ ".mps"
-  where tell' :: String -> Displayer ()
-        tell' s = tell [select mp s]   
-        rec :: Tex a -> Displayer a
-        rec = display fname mp
-        
-sho :: Show a => a -> Tex ()
-sho = Tex . show
-                             
 render :: Tex a -> IO ()                   
-render t = do 
+render (Tex t) = do
   fname <- getProgName
-  let (_,xs) = evalRWS (fromDisplayer $ display fname False t) () emptyRefs 
-      (mp,tex) = partitionEithers xs
-  writeFile (fname <.> "tex") (concat tex)
-  writeFile (fname <.> "mp") (concat mp)
+  writeToDisk (Target (fname <.> "tex") $ runReaderT t fname)
                     
-
-texLn :: String -> Tex ()    
-texLn s = Tex s >> Tex "\n"
+texLn :: String -> TeX
+texLn s = tex s >> tex "\n"
 
 texLines :: [String] -> Tex ()
 texLines = mapM_ texLn
 
 genParen :: String -> Tex a -> Tex a
-genParen [l,r] x = Tex [l] *> x <* Tex [r]
+genParen [l,r] x = tex [l] *> x <* tex [r]
 
 braces :: Tex a -> Tex a
 braces = genParen "{}"
 brackets = genParen "[]"
 
-backslash = Tex ['\\']
+backslash = tex ['\\']
 
-nil = braces (Tex "")
+nil :: TeX
+nil = braces (tex "")
 
 cmd0 c = cmdn' c [] [] >> return ()
 
@@ -134,18 +73,18 @@ cmd' cmd options arg = do
 
 cmdn' :: String -> [String] -> [Tex a] -> Tex [a]
 cmdn' cmd options args = do 
-  backslash >> Tex cmd
+  backslash >> tex cmd
   when (not $ null options) $ brackets $ sequence_ $ map tex $ intersperse "," options
   res <- sequence $ map braces args
-  when (null args) $ Tex "{}" -- so that this does not get glued with the next thing.
+  when (null args) $ tex "{}" -- so that this does not get glued with the next thing.
   return res
 
 cmdm :: String -> [Tex a] -> [Tex a] -> Tex [a]
 cmdm cmd options args = do 
-  backslash >> Tex cmd
+  backslash >> tex cmd
   when (not $ null options) $ sequence_ $ map brackets $ options
   res <- sequence $ map braces args
-  when (null args) $ Tex "{}" -- so that this does not get glued with the next thing.
+  when (null args) $ tex "{}" -- so that this does not get glued with the next thing.
   return res
 
 
@@ -157,14 +96,14 @@ cmdn_ cmd args = cmdn'_ cmd [] args
 
 env :: String -> Tex a -> Tex a
 env e body = do
-  cmd "begin" $ Tex e
+  cmd "begin" $ tex e
   x <- body
-  cmd "end" $ Tex e
+  cmd "end" $ tex e
   return x
 
 label = do
-  l <- Label
-  cmd "label" (Refer l)
+  l <- newLabel
+  cmd "label" (reference l)
   
 xref l = do
-  cmd "ref" (Refer l)
+  cmd "ref" (reference l)
