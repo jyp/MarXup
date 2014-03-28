92c73ee8648c3bb23c1c34e35d294d03c90bf1d0
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2014-03-28 11:30:11 +0100

    some steps towards graphviz support

--- MarXup/Diagram/Graphviz.hs
+++ MarXup/Diagram/Graphviz.hs
@@ -1,12 +1,22 @@
+{-# LANGUAGE RecordWildCards #-}
 module MarXup.Diagram.Graphviz where
 
-
+import MarXup.Diagram.Layout
 import Data.GraphViz
 import Data.GraphViz.Parsing
+import Data.GraphViz.Commands.IO
+import System.IO.Unsafe (unsafePerformIO)
+import Data.Traversable
+import Data.Foldable
 
 layout :: (Ord n, PrintDot n, ParseDot n) => GraphvizCommand -> DotGraph n -> DotGraph n
-layout cmd input = case unsafePerformIO $ graphvizWithHandle cmd input DotOutput hGetContents' of
-  Left err -> error  $ "Grapviz layout: graphviz reports: " ++ show err
-  Right out -> parse out
+layout cmd input = parseIt' $ unsafePerformIO $ graphvizWithHandle cmd input DotOutput hGetStrict 
+
+graphToDiagram :: DotGraph Int -> Dia
+graphToDiagram (DotGraph _strict _directed _grIdent (DotStmts{..})) =
+  forM_ nodeStmts $ \(DotNode nodeIdent nodeAttrs) -> do
+    error "todo"
+    
+  
 
 
