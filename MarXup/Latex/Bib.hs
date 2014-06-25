1f0ce1928437b724a7e8901836a5f5233f83a8d0
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2014-06-25 15:42:37 +0200

    bib on its own line

--- MarXup/Latex/Bib.hs
+++ MarXup/Latex/Bib.hs
@@ -9,7 +9,10 @@ bibliographystyle :: String -> TeX
 bibliographystyle x = cmd "bibliographystyle" $ tex x
 
 bibliography :: String -> TeX
-bibliography x = cmd "bibliography" $ tex x
+bibliography x = do
+  texLn "" -- some tools can detect bibliography command only if it stands on its own line.
+  cmd "bibliography" $ tex x
+  texLn ""
 
 citet,citep :: String -> TeX
 [citet,citep] = map (\c -> cmd c . tex) ["citet","citep"]
