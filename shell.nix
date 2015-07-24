5418119365c9f9414291f4918effa46950c78c47
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2015-07-24 13:32:18 +0200

    nix stuff

--- shell.nix
+++ shell.nix
@@ -3,8 +3,22 @@
             overrides = self: super:
             let callPackage = self.callPackage; in {
                   glpk-hs = callPackage nix/glpk-hs.nix {};
+                  # glpk-hs = callPackage /home/bernardy/repo/glpk/default.nix {};
                   typography-geometry = callPackage nix/typography-geometry.nix {};
                   thisPackage = callPackage (import ./default.nix) {};
             };
            });
-    in haskellPackages.thisPackage.env
+    in pkgs.stdenv.mkDerivation {
+    name = haskellPackages.thisPackage.name;
+    buildInputs = 
+       [(haskellPackages.ghcWithPackages (hs: ([
+         hs.cabal-install
+         hs.hdevtools
+         hs.alex
+         hs.happy
+         hs.hscolour
+       ] ++ haskellPackages.thisPackage.propagatedNativeBuildInputs)))];
+    shellHook = ''
+      export LANG=en_US.utf8
+      '';
+     }
